2b11cb218af953c3cfd0656584b4a1f65ebdaf0d
wangshiyu <wangshiyu@safeis.cn>
2023-11-09 17:27:00 +0800

    init

---
+++ src/.umi/dumi/layout.tsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import React from 'react';
+import config from '@@/dumi/config';
+import demos from '@@/dumi/demos';
+import apis from '@@/dumi/apis';
+import Layout from '/Users/shiyuwang/home/me/apps/user-center-frontend-private/node_modules/@umijs/preset-dumi/lib/theme/layout';
+
+export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/shiyuwang/home/me/apps/user-center-frontend-private/node_modules/react-helmet';
